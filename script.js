7065ce9d425de5a38a7886d3877f77692c44c3d2
Vicente Escobar <vicente_escobar@yahoo.com>
2022-02-19 21:10:52 -0600

    Agregamos los archivos index.html style.css script.js

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * years * rate /100;
-var year = new Date().getFullYear()+parseInt(years);
-function updateRate()
+function compute()
 {
-	var rateval = document.getElementById("rate").value;
-	document.getElementById("rate_val").innerText=rateval;
+	if(principal.value < 1){
+        alert("Amount has to be a positive quantity")
+        principal.focus()
+        return
+    }
+    let interest = principal.value * years.value * rate.value / 100;
+    yearInFuture = new Date().getFullYear() + parseInt(years.value);
+    result = document.getElementById("result")
+    result.innerHTML = "<p>If you deposit <mark>"+ principal.value +"</mark>, an interest rate of <mark>" + rate.value + "%</mark>.<br>You will receive an amount of <mark>"+interest+"</mark>, in the year <mark>"+yearInFuture+"</mark></p>";
+
+}
+
+function refreshSlider(){
+rate.value
+document.getElementById("rate_label").innerText = rate.value + "%"}
+
+finishPage = () =>{
+    var rate = document.getElementById("rate");
+    var principal = document.getElementById("principal");
+    var years = document.getElementById("years");
+    years = document.getElementById("years");
+    for(i=1; i<=10; i++){
+    	let element = document.createElement("option")
+    	element.innerText = i
+    	element.setAttribute("value", i)
+    	years.appendChild(element)
+    }
 }
